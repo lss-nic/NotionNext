c5210e2b02af38f6ad4536a2e07a1a796e49bd8f
shangshu liu <70892833+lss-nic@users.noreply.github.com>
2025-02-21 15:12:24 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -4,7 +4,7 @@ import { siteConfig } from '@/lib/config'
  * 驱动版权
  * @returns
  */
-export default function PoweredBy(props) {
+//export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
